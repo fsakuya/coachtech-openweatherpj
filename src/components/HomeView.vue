51418438fdd8ca8b3ac855694798e842c3ee389d
fsakuya <futamori.1991@gmail.com>
2023-04-16 17:03:31 +0900

    second-commit

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -1,49 +1,49 @@
 <template>
-  <div class="home flex">
-    <Card v-for="(data, index) in lists" :key="index" :city="data.city" />
-  </div>
+<div class="home flex">
+<Card v-for="(data, index) in lists" :key="index" :city="data.city" />
+</div>
 </template>
 
 <script>
 import Card from "../components/TheCard";
 export default {
-  data() {
-    return {
-      lists: [
-        {
-          city: "sapporo",
-        },
-        {
-          city: "tokyo",
-        },
-        {
-          city: "nagoya",
-        },
-        {
-          city: "osaka",
-        },
-        {
-          city: "fukuoka",
-        },
-        {
-          city: "naha",
-        },
-      ],
-    };
-  },
-  components: {
-    Card,
-  },
+data() {
+return {
+lists: [
+{
+city: "sapporo"
+},
+{
+city: "tokyo"
+},
+{
+city: "nagoya"
+},
+{
+city: "osaka"
+},
+{
+city: "fukuoka"
+},
+{
+city: "naha"
+}
+]
+};
+},
+components: {
+Card
+}
 };
 </script>
 
 <style scoped>
 body {
-  background: #eee;
+background: #eee;
 }
 .flex {
-  display: flex;
-  justify-content: space-around;
-  flex-wrap: wrap;
+display: flex;
+justify-content: space-around;
+flex-wrap: wrap;
 }
 </style>
